7f5a723f06b683b872e29b77207576dbc579a40b
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-28 18:45:02 +0530

    using packs as two folder and import in Testdefault

---
+++ Testdefault.java
@@ -0,0 +1,8 @@
+import mypack.util.Myclass;
+
+public class Testdefault {
+    public static void main(String[] args) {
+        Myclass obj = new Myclass();
+        obj.display();
+    }   
+}
---
+++ mypack/util/Myclass.java
@@ -0,0 +1,8 @@
+package mypack.util;
+public class Myclass {
+    public void display() 
+    {
+        System.out.println("Hello from Myclass in mypack.util");
+    }
+    
+}
