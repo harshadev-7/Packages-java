9937b4d4483302b55ab7a1e60843100a7d714d6d
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-28 18:39:22 +0530

    Sample user defined package

---
+++ Testclass.java
@@ -0,0 +1,8 @@
+import pack.Myclass;
+
+public class Testclass {
+    public static void main(String[] args) {
+        Myclass obj = new Myclass();
+        obj.display();
+    }   
+}
---
+++ pack/Myclass.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class Myclass 
+{
+    public void display() 
+    {
+        System.out.println("Hello from Myclass in userdefinedpackage.pack");
+    }
+    
+}
