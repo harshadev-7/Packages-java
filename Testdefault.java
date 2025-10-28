f881de3272cb240a4ccd5c6d2273c4e5cf30ab60
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-28 18:55:18 +0530

    After adding constructor to a package

--- Testdefault.java
+++ Testdefault.java
@@ -1,8 +1,12 @@
 import mypack.util.Myclass;
+import mypack.util.mypackage.Constructor;
 
 public class Testdefault {
     public static void main(String[] args) {
         Myclass obj = new Myclass();
         obj.display();
-    }   
+
+        Constructor cons = new Constructor(10);
+        cons.display();
+    }
 }
---
+++ mypack/util/mypackage/Constructor.java
@@ -0,0 +1,12 @@
+package mypack.util.mypackage;
+
+public class Constructor {
+    private int a;
+
+    public Constructor(int a) {
+        this.a = a;
+    }
+    public void display() {
+        System.out.println("Value of a: " + a);
+    }
+}
